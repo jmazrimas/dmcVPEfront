5ec812b2d1b17cac172a82ac90f772b8f4a22fda
jmazrimas <joe.mazrimas@gmail.com>
2017-05-07 18:12:58 -0500

    able to add to list of inputs and concat with generic title

--- inlinejs.js
+++ inlinejs.js
@@ -20,14 +20,37 @@ var returnGroupedInputs = function(group) {
 
 	for (var i=0; i<inputs.length; i++) {
 		var inputPair = {}
-		inputPair[inputs[i].id] = inputs[i].value
-		jsonInputs.push(inputPair)
+		if (!$(inputs[i]).hasClass('addedInput')) {
+			inputPair[inputs[i].id] = inputs[i].value
+			jsonInputs.push(inputPair)	
+		} else if ($(inputs[i]).hasClass('addedInputValue')){
+			inputPair['addedThing'] = inputs[i].value
+			jsonInputs.push(inputPair)	
+		}
 	}
 
 	return (JSON.stringify(jsonInputs))
 }
 
-	
+var inputTemplate;
+
+var buildInputTemplate = function(exampleInput) {
+
+	var title = exampleInput.find('.title');
+	exampleInput.find('.unit').remove();
+	exampleInput.find('.key').remove();
+	exampleInput.find('.help').remove();
+	exampleInput.find('input').attr("placeholder", "Value of Input");
+	exampleInput.find('input').addClass('addedInput addedInputValue');
+
+	$("<input class=\"addedInput\" placeholder=\"Name of Input\">").insertAfter(title);
+	title.remove();
+
+	inputTemplate = exampleInput;
+
+	console.log(inputTemplate)
+}
+
 $( document ).ready(function() {
 
 	//Listen all all "tagged" groups
@@ -41,7 +64,9 @@ $( document ).ready(function() {
 
 	//Listen on add buttons
 	$("button.addInput").click(function(event){
-		$( "<h1>Test</h1>" ).insertAfter(event.target);
+		inputTemplate.insertAfter(event.target);
 	});
+
+	buildInputTemplate($("#vpe").find(".item").first().clone());
 
 });

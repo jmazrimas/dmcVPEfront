842657e1e28808293a1c2bd94f5a2b9d8f56570b
jmazrimas <joe.mazrimas@gmail.com>
2017-05-07 17:38:07 -0500

    removed funcs from doc ready

--- inlinejs.js
+++ inlinejs.js
@@ -1,42 +1,47 @@
+var userEditListener = function(event) {
+	updateGroupInputValue(event.currentTarget);
+}
 
+//On changes, re-write variable value
+var updateGroupInputValue = function(group) {
+	group = $(group)
 
+	var groupTitle = group.find('h3')[0].innerText+"inputList";
+	var inputString = returnGroupedInputs(group);
+
+	console.log(groupTitle)
+	console.log(inputString)
+
+}
+
+var returnGroupedInputs = function(group) {
+	var inputs = group.find('input')
+	var jsonInputs = []
+
+	for (var i=0; i<inputs.length; i++) {
+		var inputPair = {}
+		inputPair[inputs[i].id] = inputs[i].value
+		jsonInputs.push(inputPair)
+	}
+
+	return (JSON.stringify(jsonInputs))
+}
+
+	
 $( document ).ready(function() {
-    
-
-	var userEditListener = function(event) {
-		updateGroupInputValue(event.currentTarget);
-	}
-
-	//On changes, re-write variable value
-	var updateGroupInputValue = function(group) {
-		group = $(group)
-
-		var groupTitle = group.find('h3')[0].innerText+"inputList";
-		var inputString = returnGroupedInputs(group);
-
-		console.log(groupTitle)
-		console.log(inputString)
-
-	}
-
-	var returnGroupedInputs = function(group) {
-		var inputs = group.find('input')
-		var jsonInputs = []
-
-		for (var i=0; i<inputs.length; i++) {
-			var inputPair = {}
-			inputPair[inputs[i].id] = inputs[i].value
-			jsonInputs.push(inputPair)
-		}
-
-		return (JSON.stringify(jsonInputs))
-	}
 
 	//Listen all all "tagged" groups
 	$(".userEditGroup").click(userEditListener).keyup(userEditListener);
+
+	//Listen on remove buttons
 	$("button.removeInput").click(function(event){
 		var inputToRemove = event.target.closest('div.item');
 		inputToRemove.remove();
+	});
+
+	//Listen on add buttons
+	$("button.addInput").click(function(event){
+		$( "<h1>Test</h1>" ).insertAfter(event.target);
 	});
 
 });
